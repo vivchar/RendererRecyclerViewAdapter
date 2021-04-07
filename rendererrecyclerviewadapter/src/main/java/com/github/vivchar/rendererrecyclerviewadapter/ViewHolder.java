b66ade5963e738acc3b67bab651700e1d6993170
Aleksey Ushakov <uas@tomsksoft.com>
2021-04-07 12:53:53 +0700

    Pass ClassCastException to WrongViewFinderException
    That way it is easier to understand the actual problem in case you got this crash on Crashlytcis, for example.

--- rendererrecyclerviewadapter/src/main/java/com/github/vivchar/rendererrecyclerviewadapter/ViewHolder.java
+++ rendererrecyclerviewadapter/src/main/java/com/github/vivchar/rendererrecyclerviewadapter/ViewHolder.java
@@ -29,7 +29,7 @@ public class ViewHolder <VF extends ViewFinder> extends RecyclerView.ViewHolder 
 			try {
 				mViewFinder = (VF) ViewFinderFactory.create(itemView);
 			} catch (ClassCastException e) { //TODO vivchar: by some reason it does not catch
-				throw new WrongViewFinderException();
+				throw new WrongViewFinderException(e);
 			}
 		}
 		return mViewFinder;
